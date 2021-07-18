20be1bb460155031e45ca4178c98c9ab63ea8666
Alber2002 <86240127+Alber2002@users.noreply.github.com>
2021-07-18 18:25:07 -0500

    ingresar y mostrar los datos personales
    agregué al archivo el código necesario para pedir y mostrar datos personales, primeramente encapsulé(private), los datos personales en variables(nombres, apellidos, cedula; fa[3],fn[3], edad[3];peso, estatura, imc), después de encapsular los datos utilicé los métodos(public), pedirle al usuario que digite sus datos personales (cedula, nombres, fecha de nacimiento, peso, estatura) con una función void, después utilicé otra función void para mostrar los datos que ingresó el usuario, organizándolos con tabuladores.

--- codigos.h
+++ codigos.h
@@ -1,1 +1,43 @@
-
+class Persona{
+	
+	private:
+		string nombres, apellidos, cedula;
+		int fa[3],fn[3], edad[3]; 
+		float peso, estatura, imc;
+		
+	public:
+		Persona (){
+				
+		}
+		void ingresar()
+		{
+			cout<<"\n\ningrese los datos"<<endl<<endl;
+			cout<<"degite su numero de cedula: ";cin>>cedula;
+			cout<<"escriba sus nombres:  ";getline(cin,nombres);
+			cout<<"escriba sus apellidos:  ";getline(cin,apellidos);
+			cout<<"digite su peso (kg):  ";cin>>peso;
+			cout<<"digite su estatura(m): ";cin>>estatura;
+			cout<<"digite su fecha de nacimiento(aa mm dd):  ";cin>>fn[0];cin>>fn[1];cin>>fn[2];
+		}
+		
+		void mostrar()
+		{
+		cout<<"\n----------------------------Datos-----------------------------\n\n";	
+		cout<<"CEDULA"<<"\t";
+		cout<<"NOMBRES"<<"\t";
+		cout<<"APELLIDOS"<<"\t";
+		cout<<"PESO"<<"\t";
+		cout<<"ESTATURA"<<"\t";
+		cout<<"FECHA DE NACIMIENTO"<<endl;
+		
+		cout<<cedula<<"\t";
+		cout<<nombres<<"\t";
+		cout<<apellidos<<"\t";
+		cout<<peso<<"\t";
+		cout<<estatura<<"\t";
+		cout<<fn[0]<<"\t";
+		cout<<fn[1]<<"\t";
+		cout<<fn[2]<<"\t";
+		
+		}
+  };
